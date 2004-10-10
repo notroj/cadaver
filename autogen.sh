319c5289bf8e86511555dd1553a74b9f75aa3316
Joe Orton <notroj@users.noreply.github.com>
2004-10-10 21:00:55 +0000

    Move .cvsignore to svn:ignore; instate externals.
    * autogen.sh: Grab neon autoconf macros from m4/neon external.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 rm -rf config.cache autom4te*.cache aclocal.m4
-${ACLOCAL:-aclocal} -I macros -I m4
+${ACLOCAL:-aclocal} -I m4 -I m4/neon
 ${AUTOHEADER:-autoheader}
 ${AUTOCONF:-autoconf}
 rm -rf autom4te*.cache
