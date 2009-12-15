2f90ce673b276ffbd634b8b7034c4e5721f445b9
joe <joe@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2009-12-15 22:35:28 +0000

    Tag release 0.23.3.

---
+++ cadaver/tags/0.23.3/autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -ex
+rm -rf config.cache autom4te*.cache aclocal.m4
+${ACLOCAL:-aclocal} -I m4 -I m4/neon
+${AUTOHEADER:-autoheader}
+${AUTOCONF:-autoconf}
+rm -rf autom4te*.cache
+rm -rf lib/intl
+cp -r /usr/share/gettext/intl lib/intl
+for f in config.guess config.sub; do 
+  cp -p /usr/share/libtool/config/$f .
+done
+# Set correct top_builddir for bundled gettext
+sed -i '/^top_builddir/s,\.\.,../..,;s,\.\./config.h,../../config.h,g' \
+       lib/intl/Makefile.in
