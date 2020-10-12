4cbea82aa20db518400a2c129781016a5ad29e49
Joe Orton <jorton@redhat.com>
2020-10-12 11:15:23 +0100

    * configure.ac, autogen.sh: Fix support for current neon.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 rm -rf config.cache autom4te*.cache aclocal.m4
-${ACLOCAL:-aclocal} -I m4 -I lib/neon/macros
+${ACLOCAL:-aclocal} -I m4 -I m4/neon
 ${AUTOHEADER:-autoheader}
 ${AUTOCONF:-autoconf}
 ${LIBTOOLIZE:-libtoolize} --copy --force >/dev/null
