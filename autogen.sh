3b137c1cd204d97434f9323f0e0efbecdd465e6d
Joe Orton <joe@manyfish.uk>
2024-01-27 10:20:57 +0000

    Add neon as a submodule.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 rm -rf config.cache autom4te*.cache aclocal.m4
-${ACLOCAL:-aclocal} -I m4 -I m4/neon
+${ACLOCAL:-aclocal} -I m4 -I neon/macros
 ${AUTOHEADER:-autoheader}
 ${AUTOCONF:-autoconf}
 ${LIBTOOLIZE:-libtoolize} --copy --force >/dev/null
